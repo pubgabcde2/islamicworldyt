4d2939fce77d2cf6babbc7812055f8f91ac3c328
Hassan <pubgabcde2@gmail.com>
2026-01-09 02:39:59 -0800

    location fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ const Page = () => {
         data-cfasync={false}
         src="https://pl28436066.effectivegatecpm.com/5cca2bf6333e364208866cc2b56d1506/invoke.js"
       ></script>
-      <div id="container-5cca2bf6333e364208866cc2b56d1506"></div>
       <div className="flex flex-col gap-10 justify-center items-center min-h-screen p-15">
         <h1 className="font-bold text-2xl sm:text-3xl">
           پڑھیں اور ثواب کمائیں۔
@@ -16,6 +15,7 @@ const Page = () => {
         <Image src="/ayat-ul-kursi.jpg" width={400} height={400} alt="ayat" />
         <Image src="/darood.png" width={400} height={400} alt="ayat" />
       </div>
+      <div id="container-5cca2bf6333e364208866cc2b56d1506"></div>
       <script src="https://pl28436125.effectivegatecpm.com/24/e6/56/24e656d59cb158131bfcdbdced87855d.js"></script>
       <script src="https://pl28436150.effectivegatecpm.com/20/1b/7e/201b7e9d7d649f67030a0abdb609fcab.js"></script>
     </>
