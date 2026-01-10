e6b5262bdd3615b792253a2a72802d06ac9dc94a
Hassan <pubgabcde2@gmail.com>
2026-01-10 01:45:01 -0800

    extra ad addeed

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ const Page = () => {
       <div id="container-5cca2bf6333e364208866cc2b56d1506"></div>
       <script src="https://pl28436125.effectivegatecpm.com/24/e6/56/24e656d59cb158131bfcdbdced87855d.js"></script>
       <script src="https://pl28436150.effectivegatecpm.com/20/1b/7e/201b7e9d7d649f67030a0abdb609fcab.js"></script>
+      {/* 1st banner */}
       <Script
         id="atoptions"
         strategy="afterInteractive"
@@ -42,9 +43,113 @@ const Page = () => {
           `,
         }}
       />
-
       <Script
         src="https://www.highperformanceformat.com/10ca9c824ca15df3e529a68b46e24478/invoke.js"
+        strategy="afterInteractive"
+      />
+
+      {/* 2nd banner */}
+      <Script
+        id="atoptions"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'ca88a2bba97ef7cb054c5534d3cd3104',
+              'format' : 'iframe',
+              'height' : 60,
+              'width' : 468,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://www.highperformanceformat.com/ca88a2bba97ef7cb054c5534d3cd3104/invoke.js"
+        strategy="afterInteractive"
+      />
+
+      {/* 3rd banner */}
+      <Script
+        id="atoptions"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'b7727671b4a72dc483c21e120dac565c',
+              'format' : 'iframe',
+              'height' : 300,
+              'width' : 160,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://www.highperformanceformat.com/b7727671b4a72dc483c21e120dac565c/invoke.js"
+        strategy="afterInteractive"
+      />
+
+      {/* 4rd banner */}
+      <Script
+        id="atoptions"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '38444a5e047795f4c887b1fcb47a43e5',
+              'format' : 'iframe',
+              'height' : 250,
+              'width' : 300,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://www.highperformanceformat.com/38444a5e047795f4c887b1fcb47a43e5/invoke.js"
+        strategy="afterInteractive"
+      />
+
+      {/* 5rd banner */}
+      <Script
+        id="atoptions"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '62a71fae8aa7e73449a854bf48b9fb31',
+              'format' : 'iframe',
+              'height' : 600,
+              'width' : 160,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://www.highperformanceformat.com/62a71fae8aa7e73449a854bf48b9fb31/invoke.js"
+        strategy="afterInteractive"
+      />
+
+      {/* 6rd banner */}
+      <Script
+        id="atoptions"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'f5059876af22738e5d7650a940491faa',
+              'format' : 'iframe',
+              'height' : 90,
+              'width' : 728,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://www.highperformanceformat.com/f5059876af22738e5d7650a940491faa/invoke.js"
         strategy="afterInteractive"
       />
     </>
